6b62de9875b59871c583d8b1c04070cc477601ec
Jez Humble <jez@jezhumble.net>
2018-03-17 17:18:28 -0700

    First commit

---
+++ src/main/java/edu/berkeley/DropwizardExampleApplication.java
@@ -0,0 +1,16 @@
+package edu.berkeley;
+
+import io.dropwizard.Application;
+import io.dropwizard.Configuration;
+import io.dropwizard.setup.Environment;
+
+public class DropwizardExampleApplication extends Application<Configuration> {
+    public static void main(String[] args) throws Exception {
+        new DropwizardExampleApplication().run(args);
+    }
+
+    @Override
+    public void run(Configuration configuration, Environment environment) {
+        environment.jersey().register(new Resource());
+    }
+}
